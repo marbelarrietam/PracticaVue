20c2e5130115a24213fe453d3031c93347afeb04
Marbel <marbelarrietam@gmail.com>
2018-12-05 09:20:42 -0500

    first commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+    appName: 'musicvue',
+    appKey: '8cab920e778ae5ff79b022d531900fce',
+    secret: 'd7fc228823e4e9e80552ad0d1000ffc1',
+    registeredTo: 'marbelarrietam'
+}
+
+
+//Application name musicvue
+//API key 8 cab920e778ae5ff79b022d531900fce
+//Shared secret d7fc228823e4e9e80552ad0d1000ffc1
+//Registered to marbelarrietam
